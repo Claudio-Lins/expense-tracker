653fd0f2de08a76d591213f979b2c04fc054968b
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-04-24 14:00:41 +0100

    List items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Item } from '../types/Item'
+import { Category } from '../types/Category'
+import { items } from '../data/items'
+import { categories } from '../data/categories'
+import { useEffect, useState } from 'react'
+import { getCurrentMonth, filterListByMonth } from '../helpers/dateFilter'
 
 const Home: NextPage = () => {
+  const [list, setList] = useState(items)
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth))
+  }, [list, currentMonth])
+
   return (
     <div id="container">
       <div id="header" className='bg-purple-700 h-40 shadow shadow-purple-700/50'>
@@ -15,6 +29,13 @@ const Home: NextPage = () => {
         {/* Input */}
         
         {/* List */}
+        {list.map((item: Item, i) => (
+          <div key={i} className='flex flex-col items-center mb-10'>
+            {item.title}
+            {item.category}
+            {item.amount}
+          </div>
+        ))}
 
       </div>
     </div>
