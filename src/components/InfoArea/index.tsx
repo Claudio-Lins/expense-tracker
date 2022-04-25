af54b8cb0480b5d814d229d8cb53ae7811560619
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-04-25 21:36:05 +0100

    data center

--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -8,7 +8,6 @@ type Props = {
 }
 
 export function InfoArea({ currentMonth, onMonthChange }: Props) {
-
   const mes = Intl.DateTimeFormat('pt-PT', { month: 'short' }).format(
     new Date(currentMonth)
   )
@@ -17,50 +16,50 @@ export function InfoArea({ currentMonth, onMonthChange }: Props) {
   )
 
   const handlePrevMonth = () => {
-    let [year, month] = currentMonth.split('-');
-    let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
-    currentDate.setMonth( currentDate.getMonth() - 1 );
-    onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
-}
+    let [year, month] = currentMonth.split('-')
+    let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1)
+    currentDate.setMonth(currentDate.getMonth() - 1)
+    onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`)
+  }
 
-const handleNextMonth = () => {
-    let [year, month] = currentMonth.split('-');
-    let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
-    currentDate.setMonth( currentDate.getMonth() + 1 );
-    onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
-}
+  const handleNextMonth = () => {
+    let [year, month] = currentMonth.split('-')
+    let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1)
+    currentDate.setMonth(currentDate.getMonth() + 1)
+    onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`)
+  }
 
   return (
-    <div className="-mt-10 flex w-full table-auto overflow-hidden rounded-lg bg-white p-5 shadow-md">
-      <div
-        id="monthArea"
-        className="flex flex-1 items-center justify-start space-x-4"
-      >
-        <BsArrowLeftSquareFill
-          onClick={handlePrevMonth}
-          size={30}
-          className="cursor-pointer text-purple-600"
-        />
-        <div className="flex flex-col items-center justify-center -space-y-1 rounded-lg border p-2 font-bold uppercase text-purple-600 shadow-md">
-          <div>
-            {mes.slice(0, 3)}
+    <>
+      <div className="w-full -mt-[80px] flex justify-center items-center mx-auto">
+        <div
+          id="monthArea"
+          className="flex items-center justify-start space-x-4"
+        >
+          <BsArrowLeftSquareFill
+            onClick={handlePrevMonth}
+            size={30}
+            className="cursor-pointer text-white"
+          />
+          <div className="flex flex-col bg-white items-center justify-center -space-y-1 rounded-lg border p-2 font-bold uppercase text-purple-600 shadow-md">
+            <div>{mes.slice(0, 3)}</div>
+            <div>{ano}</div>
           </div>
-          <div>
-            {ano}
-          </div>
+          <BsArrowRightSquareFill
+            onClick={handleNextMonth}
+            size={30}
+            className="cursor-pointer text-white"
+          />
         </div>
-        <BsArrowRightSquareFill
-          onClick={handleNextMonth}
-          size={30}
-          className="cursor-pointer text-purple-600"
-        />
       </div>
-      <div id="tituloArea" className="flex-1">
-        Titulo
+      <div className="mt-10 flex w-full table-auto overflow-hidden rounded-lg bg-white p-5 shadow-md">
+        <div id="tituloArea" className="flex-1">
+          Titulo
+        </div>
+        <div id="resumeArea" className="flex">
+          Resume
+        </div>
       </div>
-      <div id="resumeArea" className="flex">
-        Resume
-      </div>
-    </div>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,8 @@ const Home: NextPage = () => {
 
   return (
     <div id="container">
-      <div id="header" className='bg-purple-700 h-40 shadow shadow-purple-700/50'>
-        <h1 className='text-white text-center pt-8 font-bold text-3xl'>Controle Financeiro</h1>
+      <div id="header" className='bg-purple-700 h-48 shadow shadow-purple-700/50'>
+        <h1 className='text-white text-center pt-8 font-bold text-5xl drop-shadow-sm'>Controle Financeiro</h1>
       </div>
       <div id="content" className='max-w-5xl mx-auto mb-10'>
 
