126af383150b98408da312241b674a33118f1e6e
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-04-26 21:02:32 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -82,7 +82,7 @@ const Home: NextPage = () => {
           <TableArea list={filteredList} />
           <div className="flex items-center justify-center">
             <button
-              onClick={handleOpneResumeModal}
+              onClick={handleCloseResumeModal}
               className="mt-10 w-full md:w-1/3 rounded-xl bg-red-600 py-2 px-4 font-bold text-white hover:bg-purple-700"
             >
               Fechar
